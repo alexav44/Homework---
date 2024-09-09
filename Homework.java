27d841c6fc073e8efd666f4abe17b2a3c36a1f42
alexav44 <alexav44@nycstudents.net>
2024-09-09 00:43:10 +0000

    first commit

---
+++ Homework.java
@@ -0,0 +1,10 @@
+public class Homework
+{
+    public static void main(String[] args)
+    {
+        String name = "Alexa";
+        System.out.print("Hello World!");
+        System.out.println("This is " + name);
+    }
+    
+}
